02eb05b042d7f5ccb480e0a343ce9716c0039c5a
YanHuaKang <1060553506@qq.com>
2019-08-01 10:39:17 +0800

    1，点击侧边栏 刷新当前路由添加注释 2，dev环境配置添加注释

--- src/layout/components/Sidebar/Link.vue
+++ src/layout/components/Sidebar/Link.vue
@@ -18,7 +18,9 @@ export default {
   },
   methods: {
     refreshView(url) {
-      // 通过重定向空白路由页面实现当前菜单刷新
+      // 点击侧边栏 刷新当前路由，vue-element-admin官网解释
+      // https://panjiachen.gitee.io/vue-element-admin-site/zh/guide/essentials/router-and-nav.html#%E7%82%B9%E5%87%BB%E4%BE%A7%E8%BE%B9%E6%A0%8F-%E5%88%B7%E6%96%B0%E5%BD%93%E5%89%8D%E8%B7%AF%E7%94%B1
+      // 中心思想：通过重定向空白路由页面，然后再重定向到想去的页面，中间让路由url变化以实现当前页面刷新
       if (JSON.parse(sessionStorage.getItem('defaultActive')) === url) {
         // 点击的是当前路由 手动重定向页面到 '/redirect' 页面
         sessionStorage.setItem('defaultActive', JSON.stringify(url))
